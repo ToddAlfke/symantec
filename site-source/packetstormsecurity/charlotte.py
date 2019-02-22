aa458bc463ed5e7266f62eb942bc5fd08d113ef2
fox <fox@gray.local>
2019-02-22 17:44:16 -0500

    loop through all items and pass objects to csv

---
+++ site-source/packetstormsecurity/charlotte.py
@@ -0,0 +1,32 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from packetstormsecurity.items import PacketstormsecurityItem
+
+
+class CharlotteSpider(scrapy.Spider):
+    name = 'charlotte'
+    allowed_domains = ['packetstormsecurity.com']
+    start_urls = ['https://packetstormsecurity.com/files/tags/exploit/']
+
+
+    def parse(self, response):
+        all_items = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "file", " " ))]')
+        all_links = response.css('.ico:link').extract()
+        print(all_links)
+        for item in all_items:
+            title = item.css('.ico::text').extract()
+            refer = item.css('.refer *::text').extract()
+            datetime = item.css('.datetime *::text').extract()
+            detail = item.css('p::text').extract()
+            tags = item.css('.tags *::text').extract()
+            cve = item.css('.cve *::text').extract()
+            link = all_links
+            yield {
+                'title' : title,
+                'refer' : refer,
+                'datetime' : datetime,
+                'detail' : detail,
+                'tags' : tags,
+                'cve' : cve,
+                'link' : link
+            }
