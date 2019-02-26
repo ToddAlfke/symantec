ceaf3ad0139b10b3a0f526ddb24cef3dca52c4bd
fox <fox@gray.local>
2019-02-26 13:46:19 -0500

    update charlotte.py and add settings.py for pipeline setup to ElascticSearch

--- site-source/packetstormsecurity/charlotte.py
+++ site-source/packetstormsecurity/charlotte.py
@@ -1,30 +1,37 @@
+## Import required python libraries and modules such as ItemLoader and Item Class
 import scrapy
 from scrapy.loader import ItemLoader
 from packetstormsecurity.items import PacketstormsecurityItem
 
-
+## Setup Crawl with allowed_domains and start_urls to be Crawled
 class CharlotteSpider(scrapy.Spider):
     name = 'charlotte'
     allowed_domains = ['packetstormsecurity.com']
     start_urls = ['https://packetstormsecurity.com/files/tags/exploit/']
 
-
+## Define xpath or css selectors for data scrape
+## Assign scraped data to Items fields via ItemLoader
+## See items.py for declared Items fields for ingestion Pipline
     def parse(self, response):
         all_items = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "file", " " ))]')
-        for item in all_items:
-            title = item.css('.ico::text').extract()
-            refer = item.css('.refer *::text').extract()
-            datetime = item.css('.datetime *::text').extract()
-            detail = item.css('p::text').extract()
-            tags = item.css('.tags *::text').extract()
-            cve = item.css('.cve *::text').extract()
-            link = item.css('.ico::attr(href)').extract()
-            yield {
-                'title' : title,
-                'refer' : refer,
-                'datetime' : datetime,
-                'detail' : detail,
-                'tags' : tags,
-                'cve' : cve,
-                'link' : link
-            }
+        for items in all_items:
+            title = items.css('.ico::text').extract()
+            refer = items.css('.refer *::text').extract()
+            datetime = items.css('.datetime *::text').extract()
+            detail = items.css('p::text').extract()
+            tags = items.css('.tags *::text').extract()
+            cve = items.css('.cve *::text').extract()
+            link = items.css('.ico::attr(href)').extract()
+
+## Yield all scraped Items
+## Items will be imported into ElasticSearch via Scrapy ElasticSearch
+## See settings.py line item ITEM_PIPELINES for ElasticSearch import setting
+            item = PacketstormsecurityItem()
+            item['title'] = title
+            item['refer'] = refer
+            item['datetime'] = datetime
+            item['detail'] = detail
+            item['tags'] = tags
+            item['cve'] = cve
+            item['link'] = link
+            yield item
