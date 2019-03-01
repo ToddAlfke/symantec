b94fc9864679365b5f25c9777c1d98907ff49755
fox <fox@gray.local>
2019-03-01 10:43:47 -0500

    updated netsparker charlotte spider

--- site-source/netsparker/charlotte.py
+++ site-source/netsparker/charlotte.py
@@ -1,13 +1,3 @@
-import scrapy
-
-class CharlotteSpider(scrapy.Spider):
-    name = 'charlotte'
-    allowed_domains = ['netsparker.com']
-    start_urls = ['http://netsparker.com/']
-
-    def parse(self, response):
-        pass
-
 ## Import required python libraries and modules such as ItemLoader and Item Class
 import scrapy
 from scrapy.loader import ItemLoader
@@ -23,25 +13,18 @@ class CharlotteSpider(scrapy.Spider):
 ## Assign scraped data to Items fields via ItemLoader
 ## See items.py for declared Items fields for ingestion Pipline
     def parse(self, response):
-        all_items = response.css('td')
+        all_items = response.css('tr')
         for items in all_items:
-            title = items.css('.ico::text').extract()
-            refer = items.css('.refer *::text').extract()
-            datetime = items.css('.datetime *::text').extract()
-            detail = items.css('p::text').extract()
-            tags = items.css('.tags *::text').extract()
-            cve = items.css('.cve *::text').extract()
-            link = items.css('.ico::attr(href)').extract()
+            ns_id = items.css('td::text').extract()
+            name = items.css('td > a::text').extract()
+            link = items.css('td > a::attr(href)').extract()
 
 ## Yield all scraped Items
 ## Items will be imported into ElasticSearch via Scrapy ElasticSearch
 ## See settings.py line item ITEM_PIPELINES for ElasticSearch import setting
-            item = PacketstormsecurityItem()
-            item['title'] = title
-            item['refer'] = refer
-            item['datetime'] = datetime
-            item['detail'] = detail
-            item['tags'] = tags
-            item['cve'] = cve
+            item = NetsparkerItem()
+            item['ns_id'] = title
+            item['name'] = refer
             item['link'] = link
+#            item['datetime'] = datetime
             yield item
