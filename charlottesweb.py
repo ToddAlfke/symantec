24a71595104ba72ca067df50fdf13f50f33d8dcb
fox <fox@gray.local>
2019-03-05 12:29:10 -0500

    updated charlottesweb

--- charlottesweb.py
+++ charlottesweb.py
@@ -1,25 +1,3 @@
-#from packetstormsecurity import charlottesweb
-
-#from subprocess import call
-#call(["cd packetstormsecurity/", "python charlottesweb.py"])
-#import os
-#os.system('scrapy crawl charlotte')
-#import os
-#from subprocess import Popen
-#Popen("scrapy crawl charlotte", shell=True, cwd="packetstormsecurity/")
-
-#import os
-#import subprocess, os
-
-#projects = '/home/ubuntu/site-source/packetstormsecurity/'
-#projects = [name for name in os.listdir(".") if os.path.isdir(name)]
-#print(projects)
-
-#subprocess.Popen('scrapy crawl charlotte', env=dict(os.environ, PATH=projects))
-
-#Popen("scrapy crawl charlotte", shell=True, env=projects)
-
-
 #### ##################################################
 import subprocess
 import glob
@@ -27,6 +5,3 @@ import glob
 
 for project in glob.glob('site-source/*'):
     subprocess.call('scrapy crawl example', shell=True, cwd=project)
-
-
-##################### this code yo!!!!!!!!!!!!!!!!!
