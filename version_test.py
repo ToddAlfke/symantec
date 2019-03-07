eeee5ed80741d8621f5920c3a90ce0de71e5bca3
fox <fox@gray.local>
2019-03-07 12:34:39 -0500

    version check testing

--- version_test.py
+++ version_test.py
@@ -1,54 +1,53 @@
+import sys
 from elasticsearch import Elasticsearch
 from elasticsearch import Elasticsearch,helpers
 from elasticsearch.helpers import scan
-from elasticsearch_dsl import Search
+sys.path.append('/home/ubuntu/')
+from ElasticsearchConfig import *
+#################################
 
-es = Elasticsearch('18.205.185.89')
+es = Elasticsearch(ElasticsearchIP)
+
+#################################
 
 
-
-#############################################################################################################
-
-
-list_id=helpers.scan(es,query={"query":{"match_all": {}}},scroll='1m',index="packetstormsecurity", doc_type="items")
-IDs=[item_id['_id'] for item_id in list_id]
-#print(IDs)
-#test=es.search(index='packetstormsecurity', filter_path=['hits.hits._*'])
-#print(version)
-
-#def all_id():
-#    for id in IDs:
-#        res = es.get(index="packetstormsecurity", doc_type='items', id=id)
-#        print(id)
-#def all_id():
+list_id = helpers.scan(es,query={"query":{"match_all": {}}},scroll='1m',index='packetstormsecurity', doc_type="items")
+IDs = [item_id['_id'] for item_id in list_id]
 for item_id in IDs:
-#res = es.get(index="packetstormsecurity", doc_type='items', id='466cdc1caa1df263d9309b2595cde2e4691d9047')   ####### item_id)
-#version_check=[item_version[1] for item_version in res]
-#print(version_check)
-#all_id()
+    res = es.get(index='packetstormsecurity', doc_type='items', id=item_id)
+    version = (res["_version"])
+    source = (res["_source"])
+#    if version == 1:
+    print(version)
 
 
-############### This is now working for version from direct stated id number IE: > id='466cdc1caa1df263d9309b2595cde2e4691d9047
-    test = es.get(index="packetstormsecurity", doc_type='items', id=item_id)
-#print(test["_version"])
-    wow = (test["_version"], item_id)
-    print(wow)
+################  TESTING  ################
+#def project():
+#    list_id = helpers.scan(es,query={"query":{"match_all": {}}},scroll='1m',index='project', doc_type="items")
+#    IDs = [item_id['_id'] for item_id in list_id]
+#    for item_id in IDs:
+#        res = es.get(index=project, doc_type='items', id=item_id)
+#        version = (res["_version"])
+#        version_check = 7
+#        source = (res["_source"])
+#        if version == version_check:
+#           print(source)
+#
+#project()
 
-
-
-a = es.get(index="packetstormsecurity", doc_type='items', id='466cdc1caa1df263d9309b2595cde2e4691d9047')
-print(a)
-
-
-
-
-#        for version in res:
-#            if "'_version': 1" in version:
-#               print('holly molly')
-#        print(res)
-
-#    for answer in res:
-#        print(answer)
+################  TESTING  ################
+#Projects = ['packetstormsecurity', 'netsparker']
+#
+#for project in Projects:
+#    list_id = helpers.scan(es,query={"query":{"match_all": {}}},scroll='1m',index=project, doc_type="items")
+#    IDs = [item_id['_id'] for item_id in list_id]
+#    for item_id in IDs:
+#        res = es.get(index=project, doc_type='items', id=item_id)
+#        version = (res["_version"])
+#        version_check = 1
+#        source = (res["_source"])
+#        if version == version_check:
+#           print(source)
 
 
 ##################  THIS BLOCK OF CODE CALLS THE DOCUMENT TYPE FOR ID  ###################################
